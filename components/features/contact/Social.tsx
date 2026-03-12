dbb545635bbfd0b755d10f9fcfcef173180fc299
unpayedme <jv.sefuesca@gmail.com>
2026-03-13 01:41:20 +0800

    feat: add padding to the contact links section in Social component for improved layout

--- components/features/contact/Social.tsx
+++ components/features/contact/Social.tsx
@@ -41,7 +41,7 @@ export function Social() {
                         Connect with me
                     </div>
                 </div>
-                <div className="flex gap-5 flex-row items-center justify-around" >
+                <div className="flex gap-5 flex-row items-center justify-around py-5" >
                     {
                         contact_data.map(({icon: Icon, ...data}, index) => (
                             <Link href={data.link ? data.link : "#"} target="_blank" key={index} className="border-2 border-slate-200 rounded-md">
