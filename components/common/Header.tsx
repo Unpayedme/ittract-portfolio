899a9e68fa4372437d4a1aef5e376b9bb4dfe96b
unpayedme <jv.sefuesca@gmail.com>
2026-03-15 22:29:17 +0800

    fix: update Header and Section components for improved styling; enhance ProfileImage hover effects and adjust scrollbar animation timing

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -65,7 +65,7 @@ export function Header() {
                                 <Menu />
                             </DrawerTrigger>
                         </DrawerTitle>
-                        <DrawerContent className="text-center text-lg bg-white gap-0 flex flex-col justify-center dark:text-black m-w-full opacity-70">
+                        <DrawerContent className="text-center text-lg bg-white gap-0 flex flex-col justify-center dark:text-black m-w-full opacity-100">
                             <DrawerHeader className="flex flex-row justify-between">
                                 <DrawerDescription className="font-bold text-2xl flex justify-start text-center items-center pr-10">
                                     SEFUESCA.DEV
