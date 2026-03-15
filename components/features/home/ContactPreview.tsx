ed9823a2113e45ac98905f4fce4bb3aed4701ff4
unpayedme <jv.sefuesca@gmail.com>
2026-03-15 22:11:31 +0800

    feat: enhance ProfileImage and ContactPreview components; improve animation effects and layout styling

--- components/features/home/ContactPreview.tsx
+++ components/features/home/ContactPreview.tsx
@@ -1,21 +1,24 @@
 import { Section } from "@/components/common/Section";
 import { Button } from "@/components/ui/button";
 import { Mail } from "lucide-react";
-import { SectionHeader } from "@/components/common/SectionHeader"; 
+import { SectionHeader } from "@/components/common/SectionHeader";
 
 export function ContactPreview() {
     return (
         <Section>
-            <SectionHeader 
-                title="Get In Touch" 
-                description="While I am not actively seeking new roles at the moment, I welcome any inquiries or networking opportunities. I’ll do my best to respond to your message as soon as possible."
-                className="text-center"
-            />
-            <div className="flex justify-center items-center mt-10">
-                <Button className="h-10 ">
-                    <Mail />  Get in touch
-                </Button>
+            <div className="border-t-2 border-b-2 border-slate-200 py-20">
+                <SectionHeader
+                    title="Get In Touch"
+                    description="While I am not actively seeking new roles at the moment, I welcome any inquiries or networking opportunities. I’ll do my best to respond to your message as soon as possible."
+                    className="text-center"
+                />
+                <div className="flex justify-center items-center mt-10">
+                    <Button className="h-10 ">
+                        <Mail />  Get in touch
+                    </Button>
+                </div>
             </div>
+
         </Section>
     );
 }
